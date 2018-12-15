e6622b99060075e22f709d4c9c0faf1e6a0e2f83
sonercirit <sonercirit@gmail.com>
2018-12-15 23:05:47 +0300

    moves player out of window and adds removal of balls in case of collition

--- World.gd
+++ World.gd
@@ -4,4 +4,7 @@ func _ready():
 	OS.set_window_maximized(true)
 
 func _process(delta):
-	$Camera2D.move_local_x(1)
+	var collision = $CanvasLayer/Player.get_slide_collision(0)
+	if collision != null && collision.collider.is_class('KinematicBody2D'):
+		collision.collider.queue_free()
+	$Camera2D.move_local_x(200 * delta)
