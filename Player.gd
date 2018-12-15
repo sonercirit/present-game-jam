7958f955809fb4369f27b23bc9209cd8928c0693
sonercirit <sonercirit@gmail.com>
2018-12-15 15:38:56 +0300

    adds interpolation for rotating the player, changes placeholder with static-koi

--- Player.gd
+++ Player.gd
@@ -3,7 +3,6 @@ extends KinematicBody2D
 var motion = Vector2()
 const BASE_MOTION = 400
 var total_delta = 0
-signal reso
 
 func _physics_process(delta):
 
@@ -28,7 +27,6 @@ func _physics_process(delta):
 	var screensize = get_viewport_rect().size
 	var left_threshold = screensize.x * .25
 	var left_percent = (left_threshold - (self.position.x - left_threshold)) / left_threshold
-#	print(left_percent)
 	if left_percent > .02 || left_percent < -.02:
 		motion.x += left_percent
 
@@ -38,8 +36,7 @@ func _physics_process(delta):
 	var upper_percent = (upper_threshold - (position_y - upper_threshold)) / upper_threshold
 	if upper_percent > .02 || upper_percent < -.02:
 		motion.y += upper_percent
-#	print(upper_percent)
-#	print(total_delta)
+
 	if left_percent > .98 || left_percent < -.98 || upper_percent > .98 || upper_percent < -.98:
 		total_delta = 1
 	if total_delta > 0:
@@ -47,27 +44,28 @@ func _physics_process(delta):
 	elif total_delta != 0:
 		total_delta = 0
 
-#	print(motion.x)
-
+	var next_rotation = 0
 	if motion.x > 0 && motion.y > 0:
-		rotation_degrees = 135
+		next_rotation = 45
 	elif motion.x > 0 && motion.y < 0:
-		rotation_degrees = 45
+		next_rotation = 315
 	elif motion.x < 0 && motion.y > 0:
-		rotation_degrees = -135
+		next_rotation = 135
 	elif motion.x < 0 && motion.y < 0:
-		rotation_degrees = -45
+		next_rotation = 225
 	elif motion.x > 0:
-		rotation_degrees = 90
+		next_rotation = 0
 	elif motion.x < 0:
-		rotation_degrees = -90
+		next_rotation = 180
 	elif motion.y > 0:
-		rotation_degrees = 180
+		next_rotation = 90
 	elif motion.y < 0:
-		rotation_degrees = 0
+		next_rotation = 270
+
+	if total_delta > .2:
+		$Tween.interpolate_method(self, "look_at", self.position, Vector2(screensize.x / 2, screensize.y / 2), .6, Tween.TRANS_BACK, Tween.EASE_OUT)
+	elif rotation_degrees != next_rotation:
+		$Tween.interpolate_property(self, "rotation_degrees", self.rotation_degrees, next_rotation, .5, Tween.TRANS_BACK, Tween.EASE_OUT)
+	$Tween.start()
 
 	move_and_slide(motion.normalized() * BASE_MOTION)
-
-
-func _on_Viewport_size_changed():
-	print('size changed')
