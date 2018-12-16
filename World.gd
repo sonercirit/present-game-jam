f2e7e77c194fa0c09e6bd11897cfef54694c0e30
sonercirit <sonercirit@gmail.com>
2018-12-16 21:18:24 +0300

    adds score

--- World.gd
+++ World.gd
@@ -3,6 +3,7 @@ extends Node
 export (PackedScene) var Ball
 var total_delta = 0
 var screensize = Vector2()
+var score = 50
 
 func _ready():
 	OS.set_window_maximized(true)
@@ -12,16 +13,27 @@ func _process(delta):
 
 	total_delta += delta
 
-	if total_delta > 1:
+	if total_delta > 2:
 		total_delta = 0
-		var ball = Ball.instance()
-		ball.position = Vector2(randi() % int(screensize.y), -100)
-		print(ball.position)
-		$CanvasLayer.add_child(ball)
+		var white_ball = Ball.instance()
+		white_ball.position = Vector2(randi() % int(screensize.x), -100)
+		$CanvasLayer.add_child(white_ball)
+		var black_ball = Ball.instance()
+		black_ball.position = Vector2(randi() % int(screensize.x), screensize.y + 100)
+		black_ball.type = 'black'
+		$CanvasLayer.add_child(black_ball)
 
 	if $CanvasLayer/Player.get_slide_count() != 0:
 		var collision = $CanvasLayer/Player.get_slide_collision(0)
-		if collision != null && collision.collider.is_class('KinematicBody2D'):
+		if collision != null && collision.collider != null && collision.collider.is_class('KinematicBody2D')\
+			&& collision.collider.type != null:
 			collision.collider.queue_free()
+			var type = collision.collider.type
+			if type == 'white':
+				score += 2.5
+				$CanvasLayer/Score.text = str(score)
+			if type == 'black':
+				score -= 2.5
+				$CanvasLayer/Score.text = str(score)
 	$Camera2D.move_local_x(200 * delta)
 	
