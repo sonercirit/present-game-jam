2816811841537af391b4fa3706c8053a3f7cbf13
sonercirit <sonercirit@gmail.com>
2018-12-16 21:04:26 +0300

    adds spinning to ball when idle

--- Ball.gd
+++ Ball.gd
@@ -1,24 +1,37 @@
 extends KinematicBody2D
 
+export (String) var type
+
+var initial_target = Vector2()
 var target = Vector2()
 var x_percent = 0
 var y_percent = 0
+var screensize = Vector2()
+var at_position = false
+var stage = 0
+var stage0 = Vector2()
 
 func _ready():
-	randomize()
-	var screensize = get_viewport_rect().size
 
-	var width_min = int(screensize.x * .25)
-	var width_max = int(screensize.x * .5)
+	if type == 'black':
+		modulate = Color(0, 0, 0)
+
+	randomize()
+	screensize = get_viewport_rect().size
+
+	var width_min = int(screensize.x * .5)
+	var width_max = int(screensize.x) - width_min
 
 	var height_piece_lenght = screensize.y / 9
-	var height_min = int(height_piece_lenght * 2)
-	var height_max = int(height_piece_lenght * 4.5)
+	var height_min = int(height_piece_lenght * 2.5)
+	var height_max = int(height_piece_lenght * 6.5) - height_min
 
 	var width = randi() % width_max + width_min
 	var height = randi() % height_max + height_min
 
 	target = Vector2(width, height)
+	initial_target = Vector2(width, height)
+
 
 	var x_length = abs(position.x - target.x)
 	var y_length = abs(position.y - target.y)
@@ -26,15 +39,67 @@ func _ready():
 	x_percent = x_length / max_length
 	y_percent = y_length / max_length
 
+	$Tween.interpolate_property(self, "position", self.position, target, 10, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
 func _physics_process(delta):
 
+	if at_position:
+		stage += 1
+		at_position = false
+		var part = screensize.y / 10 / 2
+		if stage == 1:
+			target = Vector2(initial_target.x - part, initial_target.y)
+#			move_and_slide(Vector2(-1,0).normalized() * part)
+		if stage == 2:
+			target = Vector2(initial_target.x - (part * .92), initial_target.y + (part * .38))
+#			move_and_slide(Vector2(-1,1).normalized() * part)
+		if stage == 3:
+			target = Vector2(initial_target.x - (part * .7), initial_target.y + (part * .7))
+#			move_and_slide(Vector2(-1,1).normalized() * part)
+		if stage == 4:
+			target = Vector2(initial_target.x - (part * .38), initial_target.y + (part * .92))
+#			move_and_slide(Vector2(-1,1).normalized() * part)
+		if stage == 5:
+			target = Vector2(initial_target.x, initial_target.y + part)
+#			move_and_slide(Vector2(0,1).normalized() * part)
+		if stage == 6:
+			target = Vector2(initial_target.x + (part * .38), initial_target.y + (part * .92))
+#			move_and_slide(Vector2(1,1).normalized() * part)
+		if stage == 7:
+			target = Vector2(initial_target.x + (part * .7), initial_target.y + (part * .7))
+#			move_and_slide(Vector2(1,1).normalized() * part)
+		if stage == 8:
+			target = Vector2(initial_target.x + (part * .92), initial_target.y + (part * .38))
+#			move_and_slide(Vector2(1,1).normalized() * part)
+		if stage == 9:
+			target = Vector2(initial_target.x + part, initial_target.y)
+#			move_and_slide(Vector2(1,0).normalized() * part)
+		if stage == 10:
+			target = Vector2(initial_target.x + (part * .92), initial_target.y - (part * .38))
+#			move_and_slide(Vector2(1,-1).normalized() * part)
+		if stage == 11:
+			target = Vector2(initial_target.x + (part * .7), initial_target.y - (part * .7))
+#			move_and_slide(Vector2(1,-1).normalized() * part)
+		if stage == 12:
+			target = Vector2(initial_target.x + (part * .38), initial_target.y - (part * .92))
+#			move_and_slide(Vector2(1,-1).normalized() * part)
+		if stage == 13:
+			target = Vector2(initial_target.x, initial_target.y - part)
+#			move_and_slide(Vector2(0,-1).normalized() * part)
+		if stage == 14:
+			target = Vector2(initial_target.x - (part * .38), initial_target.y - (part * .92))
+#			move_and_slide(Vector2(-1,-1).normalized() * part)
+		if stage == 15:
+			target = Vector2(initial_target.x - (part * .7), initial_target.y - (part * .7))
+#			move_and_slide(Vector2(-1,-1).normalized() * part)
+		if stage == 16:
+			target = Vector2(initial_target.x - (part * .92), initial_target.y - (part * .38))
+#			move_and_slide(Vector2(-1,-1).normalized() * part)
+			initial_target = Vector2(initial_target.x - part, initial_target.y)
+			stage = 0
+		$Tween.interpolate_property(self, "position", self.position, target, .25, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+		$Tween.start()
 
-	if position.x < target.x:
-		move_local_x(50 * delta * x_percent)
-	elif position.x > target.x:
-		move_local_x(-50 * delta * x_percent)
-
-	if position.y < target.y:
-		move_local_y(50 * delta * y_percent)
-	elif position.y > target.y:
-		move_local_y(-50 * delta * y_percent)
+func _on_Tween_tween_completed(object, key):
+	at_position = true
