ecef442a0ca3752535e75d42f52642a01ee8018c
sonercirit <sonercirit@gmail.com>
2018-12-18 06:29:57 +0300

    adds extra options to HUD

--- HUD.gd
+++ HUD.gd
@@ -1,10 +1,20 @@
 extends CanvasLayer
 
 signal start_game
+signal show_credits
 
 func _ready():
 	pass
 
 func _on_Start_pressed():
-	$Start.hide()
+	hide_HUD()
 	emit_signal('start_game')
+
+func _on_Credits_pressed():
+	hide_HUD()
+	emit_signal('show_credits')
+
+func hide_HUD():
+	$Start.hide()
+	$Title.hide()
+	$Credits.hide()
